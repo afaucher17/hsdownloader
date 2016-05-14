34780b1cea659a6154134318feecf9b9656ccaec
afaucher17 <aymeric.faucher@gmail.com>
2016-05-14 17:14:02 +0200

    that's it for today

--- downloader.py
+++ downloader.py
@@ -3,22 +3,18 @@
 import sys
 import os
 
+import BeautifulSoup
+
 from html.parser import HTMLParser
 import urllib.request, urllib.parse, urllib
 from urllib.error import HTTPError, URLError
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag :", tag)
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-    def handle_data(self, data):
-        print("Encountered some data :",  data)
-
 def main(argv):
     try:
         req = urllib.request.Request('http://horriblesubs.info/lib/search.php?value=test', headers={'User-Agent': 'Mozilla/5.0'})
-        res = urllib.request.urlopen(req).read()
+        res = urllib.request.urlopen(req)
+        soup = BeautifulSoup(res)
+        elem = soup.findAll('a', {'href': 'link here'})
     except HTTPError as e:
         print("Server return " + str(e.code) + " error")
         error = e
@@ -26,8 +22,7 @@ def main(argv):
         print("Can't reach server: " + e.reason)
         error = e
     else:
-        parser = MyHTMLParser()
-        parser.feed(res.decode("utf-8"))
+        print(elem[0].href)
 
 if __name__ == "__main__":
     main(sys.argv)
